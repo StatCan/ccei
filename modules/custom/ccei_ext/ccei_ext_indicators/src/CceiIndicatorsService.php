7f4b0f6fe9e1b0b2a2c58a79d63483cb56feb3bb
Philippe T. Simard <ptsimard@gmail.com>
2020-04-02 20:39:41 -0400

    feat(indicators) Improve service with aggregated bulk query and more

--- modules/custom/ccei_ext/ccei_ext_indicators/src/CceiIndicatorsService.php
+++ modules/custom/ccei_ext/ccei_ext_indicators/src/CceiIndicatorsService.php
@@ -56,62 +56,117 @@ class CceiIndicatorsService implements CceiIndicatorsServiceInterface {
   public function getIndicatorsData() {
     // Load list of indicator parameters.
     $config = $this->configFactory->get('ccei_ext_indicators.indicators')->get('');
+    $indicators = &$config['indicators'];
 
+    // Aggregate queries from all indicators in one array.
+    // Create a lookup table to match queries with indicators.
+    $queriesAccumulator = [];
+    $queriesLookupTable = [];
+    foreach ($config['indicators'] as $key => $indicator) {
+      foreach ($indicator['sources'][0]['queries'] as $query) {
+        $queriesAccumulator[] = $query;
+        $hash = hash('adler32', $query['productId'] . $query['coordinate']);
+        $queriesLookupTable[$hash] = $key;
+
+        // Create a hashed response placeholders to maintain sort order.
+        $indicators[$key]['response'][$hash] = [];
+      }
+    }
+
+    // Call the API once with the list of queries.
+    $responses = $this->queryCoords($config['urls']['url1'], $queriesAccumulator);
+
+    // Redistribute API responses to their respective indicators, keeping order.
+    foreach ($responses as $key => $response) {
+      $hash = hash('adler32', $response['object']['productId'] . $response['object']['coordinate']);
+      $indicators[$queriesLookupTable[$hash]]['response'][$hash] = $response;
+    }
+
+    // Process each indicators.
     $results = [];
-    // For each indicator call a dedicated function for their particular case.
-    // Accumulate their results in a list.
-    foreach ($config['indicators'] as $indicator) {
-      $results[] = $this->{'process' . $indicator['id']}($config['urls'], $indicator);
+    foreach ($indicators as $key => $indicator) {
+      $results[] = $this->process($indicator);
     }
 
     // Return the list.
-    return $results;
+    return [
+      'types' => $config['types'],
+      'indicators' => $results,
+    ];
   }
 
-  private static function packageIndicator($indicator, $result) {
-    $output = [];
-    $output['title'] = $indicator['title'];
-    $output = array_merge($output, $result);
-    $output['units'] = $indicator['units'];
-
-    return $output;
-  }
-
-  private function queryVector($url, $query) {
+  private function queryCoords($url, $source) {
     // Guide: https://www.statcan.gc.ca/eng/developers/wds/user-guide#a12-4
     $request = $this->httpClient->post($url,
       [
-//        'on_stats' => function (\GuzzleHttp\TransferStats $stats) {
-//          echo $stats->getEffectiveUri() . ' : ' . $stats->getTransferTime();
-//        },
-        'json' => [
-          $query,
-        ],
+        'json' => $source,
       ]);
 
     // TODO: Implement http response status validation.
-    $response = Json::decode($request->getBody()->getContents());
-    return $response;
+    return Json::decode($request->getBody()->getContents());
   }
 
-  public static function percentChange($previous, $latest) {
-    return round(($latest - $previous) / $previous * 100, 2) . '%';
-  }
+  private function process($indicator) {
+    // Reset responses to numerical array.
+    $responseIndicators = array_values($indicator['response']);
 
-  private function processElect001($urls, $indicator) {
-    $response = $this->queryVector($urls[$indicator['sources'][0]['base_url']], $indicator['sources'][0]['query']);
+    $datapoints = [];
+    foreach ($responseIndicators as $key => $responseDatapoint) {
+      $datapoints[$key]['values'] = $responseDatapoint['object']['vectorDataPoint'];
+      $datapoints[$key]['previous'] = reset($datapoints[$key]['values'])['value'];
+      $datapoints[$key]['latest'] = end($datapoints[$key]['values'])['value'];
+    }
 
-    $values = $response[0]['object']['vectorDataPoint'];
-    $previous = reset($values)['value'] / 1000;
-    $latest = end($values)['value'] / 1000;
+    // Call specialised process function.
+    list('previous' => $previous, 'latest' => $latest) = $this->{$indicator['function']}($datapoints);
 
-    $percentChange = self::percentChange($previous, $latest);
+    if (isset($indicator['valueformat'])) {
+      $formattedValue = call_user_func_array(
+        $indicator['valueformat']['function'],
+        [$latest, $indicator['valueformat']['precision']]
+      );
+    }
 
-    $result = [
-      'change' => $percentChange,
-      'value' => round($latest),
+    $percentChange = round(($latest - $previous) / $previous * 100, 1);
+
+    return [
+      'title' => $indicator['title'],
+      'type' => $indicator['type'],
+      'change' => $percentChange . '%',
+      'direction' => $percentChange > 0 ? 'up' : ($percentChange == 0 ? 'nil' : 'down'),
+      'value' => $formattedValue ?? $latest,
+      'units' => $indicator['units'],
+      'refper' => reset($datapoints[0]['values'])['refPer'],
     ];
-    return self::packageIndicator($indicator, $result);
+  }
+
+  private function processSimple($datapoints) {
+    return [
+      'previous' => $datapoints[0]['previous'],
+      'latest' => $datapoints[0]['latest'],
+    ];
+  }
+
+  private function processDivisionThousand($datapoints) {
+    return [
+      'previous' => $datapoints[0]['previous'] / 1000,
+      'latest' => $datapoints[0]['latest'] / 1000,
+    ];
+  }
+
+  private function processAverage($datapoints) {
+    return [
+      'previous' => $datapoints[1]['previous'] / $datapoints[0]['previous'] * 100,
+      'latest' => $datapoints[1]['latest'] / $datapoints[0]['latest'] * 100,
+    ];
+  }
+
+  private function processSumTimesConstant($datapoints) {
+    // TODO: what to do if a datapoint failed to load/is null?
+    return [
+      'previous' => array_sum(array_column($datapoints, 'previous')) * 6.28981077,
+      'latest' => array_sum(array_column($datapoints, 'latest')) * 6.28981077,
+    ];
   }
 
   public function apiTest() {
