cb9fae8a533d628c2e74e3f73bc74e6d306ef95b
natew <nathanpweir@gmail.com>
2020-04-23 06:07:20 -0400

    fix(subject): Update power bi formatter to be closer to the UX designs

--- modules/custom/ccei_subject/src/Plugin/Field/FieldFormatter/PowerBIFormatter.php
+++ modules/custom/ccei_subject/src/Plugin/Field/FieldFormatter/PowerBIFormatter.php
@@ -2,8 +2,11 @@
 
 namespace Drupal\ccei_subject\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Template\Attribute;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Component\Render\HtmlEscapedText;
 use Drupal\iframe\Plugin\Field\FieldFormatter\IframeDefaultFormatter;
+use Drupal\Core\Url;
 
 /**
  * Plugin implementation of the 'power_bi_formatter' formatter.
@@ -33,7 +36,10 @@ class PowerBIFormatter extends IframeDefaultFormatter {
       if (!isset($item->title)) {
         $item->title = '';
       }
-      $elements[$delta] = self::iframeIframe($item->title, $item->url, $item);
+      // Since I can't get the iframe field foramtter settings to work in the
+      // UI. I will override it here for our specific need.
+      $elements[$delta] = self::renderIframe($item->title, $item->url, $item);
+      // Use our own theme template.
       $elements[$delta]['#theme'] = 'power_bi_formatter';
       // Tokens can be dynamic, so its not cacheable.
       if (isset($settings['tokensupport']) && $settings['tokensupport']) {
@@ -41,6 +47,59 @@ class PowerBIFormatter extends IframeDefaultFormatter {
       }
     }
     return $elements;
+  }
+
+  /**
+   * This is a hack because I couldn't get the IframeDefaultFormatter settings
+   * to work properly.
+   */
+  public static function renderIframe($text, $path, $item) {
+    $options = [];
+    $allow = [];
+    $style = '';
+
+    // Remove all HTML and PHP tags from a tooltip.
+    // For best performance, we act only
+    // if a quick strpos() pre-check gave a suspicion
+    // (because strip_tags() is expensive).
+    if  (!empty($item->title)) {
+      $options['title'] = $item->title;
+      if (strpos($options['title'], '<') !== FALSE) {
+        $options['title'] = strip_tags($options['title']);
+      }
+    }
+
+    if (\Drupal::moduleHandler()->moduleExists('token')) {
+      // Token Support for field "url" and "title".
+      $tokensupport = $item->getTokenSupport();
+      $tokencontext = ['user' => \Drupal::currentUser()];
+      if (isset($GLOBALS['node'])) {
+        $tokencontext['node'] = $GLOBALS['node'];
+      }
+      if ($tokensupport > 0) {
+        $text = \Drupal::token()->replace($text, $tokencontext);
+      }
+      if ($tokensupport > 1) {
+        $path = \Drupal::token()->replace($path, $tokencontext);
+      }
+    }
+
+    $options_link = [];
+    $options_link['attributes'] = [];
+    $options_link['attributes']['title'] = $options['title'];
+    $srcuri = Url::fromUri($path, $options_link);
+    $src = $srcuri->toString();
+    $options['src'] = $src;
+    $drupal_attributes = new Attribute($options);
+
+    $render_array = [
+      '#theme' => 'iframe',
+      '#src' => $src,
+      '#attributes' => $drupal_attributes,
+      '#text' => (isset($options['html']) && $options['html'] ? $text : new HtmlEscapedText($text)),
+      '#style' => 'iframe#' . $htmlid . ' {' . $style . '}',
+    ];
+    return $render_array;
   }
 
 }
